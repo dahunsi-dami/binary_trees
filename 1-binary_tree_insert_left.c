0ffeaa29ae05fcf4035c8cd3d709063c5a717cb7
dahunsi-dami <gbemigadare@gmail.com>
2024-03-26 16:56:38 +0100

    Fix trailing whitespace at line 30.

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -27,7 +27,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 		parent->left = newlnode;
 		newlnode->parent = parent;
 	}
-	
+
 	newlnode->n = value;
 	newlnode->right = NULL;
 
