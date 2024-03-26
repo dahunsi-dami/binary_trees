5f844eabec711a0c3f393683824e90d85ea8a3b7
dahunsi-dami <gbemigadare@gmail.com>
2024-03-26 17:01:43 +0100

    Write func to insert node as right child of a node w/ given specs.

---
+++ 2-binary_tree_insert_right.c
@@ -0,0 +1,35 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_insert_right - inserts node as right child of another node.
+ * @parent: pointer to the parent node of the node to create.
+ * @value: value to put in the new node.
+ * Return: pointer to the new node or NULL on failure/parent is NULL.
+ */
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
+{
+	binary_tree_t *newlnode;
+
+	newlnode = (binary_tree_t *)malloc(sizeof(binary_tree_t));
+
+	if (newlnode == NULL || parent == NULL)
+		return (NULL);
+
+	if (parent->right != NULL)
+	{
+		newlnode->right = parent->right;
+		parent->right = newlnode;
+		newlnode->parent = parent;
+		newlnode->right->parent = newlnode;
+	}
+	else
+	{
+		newlnode->right = NULL;
+		parent->right = newlnode;
+		newlnode->parent = parent;
+	}
+
+	newlnode->n = value;
+	newlnode->left = NULL;
+
+	return (newlnode);
+}
