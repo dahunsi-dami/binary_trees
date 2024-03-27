292232d6cc7b8f65cbd7f3f4cd7aa729bf97d2d5
dahunsi-dami <gbemigadare@gmail.com>
2024-03-27 22:51:43 +0100

    Add 13-binary*.c prototype to header & fix betty issues.

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -2,19 +2,19 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_nodes - Counts the nodes with at least one child in a binary tree
+ * binary_tree_nodes - Counts nodes with at least one child in a binary tree
  * @tree: A pointer to the root node of the tree to count the nodes
  *
  * Return: The number of nodes with at least one child, or 0 if tree is NULL
  */
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
-    if (tree == NULL)
-        return (0);
+	if (tree == NULL)
+		return (0);
 
-    if (tree->left != NULL || tree->right != NULL)
-        return (binary_tree_nodes(tree->left) + 1 + binary_tree_nodes(tree->right));
-    else
-        return (binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right));
+	if (tree->left != NULL || tree->right != NULL)
+		return (binary_tree_nodes(tree->left) + 1 + binary_tree_nodes(tree->right));
+	else
+		return (binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right));
 }
 
