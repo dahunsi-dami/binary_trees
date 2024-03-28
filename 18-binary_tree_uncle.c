d342c66df1008e0befd467ebf358c32243cdb132
dahunsi-dami <gbemigadare@gmail.com>
2024-03-28 01:33:18 +0100

    Adhere to Betty-style: indents & return () syntax.

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -6,25 +6,25 @@
  */
 binary_tree_t *tree_sibling(binary_tree_t *node)
 {
-        binary_tree_t *ln, *rn;
+	binary_tree_t *ln, *rn;
 
-        if (node == NULL || node->parent == NULL)
-                return (NULL);
+	if (node == NULL || node->parent == NULL)
+		return (NULL);
 
-        if (node->parent->left == node)
-                return (node->parent->right);
+	if (node->parent->left == node)
+		return (node->parent->right);
 
-        if (node->parent->right ==  node)
-                return (node->parent->left);
+	if (node->parent->right ==  node)
+		return (node->parent->left);
 
-        ln = tree_sibling(node->parent->left);
+	ln = tree_sibling(node->parent->left);
 
-        if (ln != NULL)
-                return (ln);
+	if (ln != NULL)
+		return (ln);
 
-        rn = tree_sibling(node->parent->right);
+	rn = tree_sibling(node->parent->right);
 
-        return (rn);
+	return (rn);
 }
 /**
  * binary_tree_uncle - finds uncle of a node.
@@ -39,5 +39,5 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 	if (node->parent->parent == NULL)
 		return (NULL);
 
-	return tree_sibling(node->parent);
+	return (tree_sibling(node->parent));
 }
